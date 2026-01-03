51cd2fd58b612bc39a156892a03a595495dbe22c
风起深空 <15827139224@163.com>
2026-01-03 21:29:50 +0800

    Add files via upload

---
+++ 字符串最后一个单词的长度.cpp
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS 
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    string line;
+//    getline(cin, line);
+//    size_t post = line.rfind(' ');
+//    if (post != string::npos)
+//    {
+//        cout << line.size() - post - 1 << endl;
+//    }
+//    else
+//    {
+//        cout << line.size() << endl;
+//    }
+//}
