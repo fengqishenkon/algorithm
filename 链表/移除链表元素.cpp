7e1caaf9a8b9993c212091e5a7db1d3cd0999c23
风起深空 <15827139224@163.com>
2026-02-28 10:54:40 +0800

    Add files via upload

---
+++ 链表/移除链表元素.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+
+//https://leetcode.cn/problems/remove-linked-list-elements/description/
+
+//虚拟头节点法
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0); // 设置一个虚拟头结点
+        dummyHead->next = head; // 将虚拟头结点指向head，这样方便后面做删除操作
+        ListNode* cur = dummyHead;
+        while (cur->next != NULL) {
+            if (cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
+};
