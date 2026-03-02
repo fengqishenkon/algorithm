ff361b6c5650766bc5a458d71a008c602b6ce6e7
风起深空 <15827139224@163.com>
2026-03-02 22:55:24 +0800

    Add files via upload

---
+++ 链表/反转链表.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+
+//https://leetcode.cn/problems/reverse-linked-list/description/
+
+//三指针法
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp; // 保存cur的下一个节点
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        while (cur) {
+            temp = cur->next;  // 保存一下 cur的下一个节点，因为接下来要改变cur->next
+            cur->next = pre; // 翻转操作
+            // 更新pre 和 cur指针
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+};
