98a7d5fcdb7a52302cc84eebb91df346e7d27d6f
风起深空 <15827139224@163.com>
2026-03-24 23:36:18 +0800

    Add files via upload

---
+++ 回溯算法/电话号码的字母组合.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 
+// 版本二
+class Solution {
+private:
+    const string letterMap[10] = {
+        "", // 0
+        "", // 1
+        "abc", // 2
+        "def", // 3
+        "ghi", // 4
+        "jkl", // 5
+        "mno", // 6
+        "pqrs", // 7
+        "tuv", // 8
+        "wxyz", // 9
+    };
+public:
+    vector<string> result;
+    void getCombinations(const string& digits, int index, const string& s) { // 注意参数的不同
+        if (index == digits.size()) {
+            result.push_back(s);
+            return;
+        }
+        int digit = digits[index] - '0';
+        string letters = letterMap[digit];
+        for (int i = 0; i < letters.size(); i++) {
+            getCombinations(digits, index + 1, s + letters[i]);  // 注意这里的不同
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        result.clear();
+        if (digits.size() == 0) {
+            return result;
+        }
+        getCombinations(digits, 0, "");
+        return result;
+
+    }
+};
